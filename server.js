c4b41fac30b8b8514a53d1b2344131ac34145a23
Francesco Codagnone <codagnone.francesco@gmail.com>
2022-03-08 17:19:30 +0100

    Initial

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+/*app.get('/', (req, res) => {
+    res.send('Heeelloooo');
+});*/
+
+app.get('/', (req, res) => {
+    res.send({
+        id: 1,
+        name: 'Sir Isaac Newton'
+    });
+});
+
+app.get('/messages', (req, res) => {
+    res.send('<ul><li>Helloo Albert!</li></ul>');
+});
+
+app.post('/messages', (req, res) => {
+    console.log('Updating messages...');
+});
+
+/*app.listen(PORT, () => {
+    console.log(`Listening to ${PORT}`);
+});*/
