f23501dd4244ac8dea3c1fd084865a222a92d695
Francesco Codagnone <codagnone.francesco@gmail.com>
2022-03-15 15:50:16 +0100

    Express routers

--- server.js
+++ server.js
@@ -1,48 +1,23 @@
 const express = require('express');
+const friendsRouter = require('./routes/friends.router');
+const messagesRouter = require('./routes/messages.router');
 
 const app = express();
 
 const PORT = 3000;
 
-const friends = [
-    {
-        id: 0,
-        name: 'Albert Einstein'
-    },
-    {
-        id: 1,
-        name: 'Sir Isaac Newton'
-    }
-];
 
-/*app.get('/', (req, res) => {
-    res.send('Heeelloooo');
-});*/
-
-app.get('/friends', (req, res) => {
-    res.json(friends);
+app.use((req, res, next) => {
+    const start = Date.now();
+    next();
+    const delta = Date.now() - start;
+    console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
-// GET /friends/22
-app.get('/friends/:friendId', (req, res) => {
-    const friendId = Number(req.params.friendId);
-    const friend = friends[friendId];
-    if (friend) {
-        res.status(200).json(friend);
-    } else {
-        res.status(404).json({
-            error: "Friend does not exist"
-        });
-    }
-});
+app.use(express.json());
 
-app.get('/messages', (req, res) => {
-    res.send('<ul><li>Helloo Albert!</li></ul>');
-});
-
-app.post('/messages', (req, res) => {
-    console.log('Updating messages...');
-});
+app.use('/friends', friendsRouter);
+app.use('/messages', messagesRouter);
 
 app.listen(PORT, () => {
     console.log(`Listening to ${PORT}`);
